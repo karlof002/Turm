fcf002fbbf8b9b5634a188b7c5ac726bd0538075
karlof002 <57101995+karlof002@users.noreply.github.com>
2021-02-04 15:02:45 +0100

    Add files via upload

---
+++ Turm.cs
@@ -0,0 +1,38 @@
+﻿/***********************************************************************************************
+ * Übungsnr:        09                                     
+ * Programmname:    Turm                                  
+ * Autor:           Michael Bucek  
+ * Klasse:          1CHIF
+ * Datum:           11.11.2013                               
+ * ------------------------------------------------ 
+ * Kurzbeschreibung:      
+ * Gibt eine Turmrechnung aus
+ * ************************************************
+*/
+
+
+using System;
+
+namespace Turm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Mit welcher Zahl wollen Sie den Turm starten?");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= 9; i++)
+            {
+                Console.WriteLine("{0,10} * {1}", number, i);
+                number = number * i;
+            }
+            for (int i = 9; i >=2; i--)
+            {
+                Console.WriteLine("{0,10} / {1}", number, i);
+                number = number / i;
+            }
+            Console.WriteLine("{0,10}",number);
+        }
+    }
+}
